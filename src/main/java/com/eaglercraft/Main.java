797fc2e9238279dd02a4f710d61f95a086823d1d
Zw9876 <zwilcox9876@gmail.com>
2026-03-25 18:58:03 -0500

    Working Java to wasm-gc pipeline with console output

--- src/main/java/com/eaglercraft/Main.java
+++ src/main/java/com/eaglercraft/Main.java
@@ -3,5 +3,6 @@ package com.eaglercraft;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Eaglercraft 1.16 - wasm-gc bootstrap");
+        System.out.println("Java is running inside wasm-gc!");
     }
 }
